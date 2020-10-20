e9f112006751a93086f040e9a185bc7852e28ba4
Sandeep Sethi <adeveloper79@gmail.com>
2020-10-20 14:18:50 +0530

    apollo: redo omni_apollo makefile

--- omni_apollo.mk
+++ omni_apollo.mk
@@ -16,23 +16,23 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Release name
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RELEASE_NAME := apollo
+DEVICE_PATH := device/xiaomi/apollo
 
-# Inherit from apollo device
-$(call inherit-product, device/xiaomi/apollo/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := apollo
 PRODUCT_NAME := omni_apollo
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := M2007J3SC
+PRODUCT_MODEL := Mi 10T Pro
 PRODUCT_MANUFACTURER := xiaomi
-PRODUCT_RELEASE_NAME := Xiaomi M2007J3SC
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
